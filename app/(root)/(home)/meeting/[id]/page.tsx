19f597d5b19b6a125044c70dbe47f7bf4d97715a
Saqib Ayoub <saqibtelixy@gmail.com>
2024-08-07 15:46:52 +0530

    vid 0.20.0

---
+++ app/(root)/(home)/meeting/[id]/page.tsx
@@ -0,0 +1,9 @@
+
+
+const Meeting = ({ params }: { params: { id: string } }) => {
+    return (
+        <div>Meeting room: #{params.id}</div>
+    )
+}
+
+export default Meeting
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
     return (
-        <div className=" bg-blue-950 flex justify-center items-center h-screen">
+        <div >
             <Button >Click me</Button>
         </div>
     )
