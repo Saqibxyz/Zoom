deed9619655ae7f50555ac5b5f9a41cb4634ad52
Saqib Ayoub <saqibtelixy@gmail.com>
2024-08-07 16:00:09 +0530

    disaster

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
     return (
-        <div >
+        <div className=" bg-blue-950 flex justify-center items-center h-screen">
             <Button >Click me</Button>
         </div>
     )
