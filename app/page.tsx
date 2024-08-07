12a12d0852a2dfd354a61fdecbc0e79c4cfe0368
Saqib Ayoub <saqibtelixy@gmail.com>
2024-08-07 15:16:44 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+    return (
+        <div className=" bg-blue-950 flex justify-center items-center h-screen">
+            <Button >Click me</Button>
+        </div>
+    )
+}
